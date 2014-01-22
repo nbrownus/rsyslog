118e142967759e31475561dd88073008b4e447ca
Nate Brown <nbrown.us@gmail.com>
2014-01-22 11:31:50 -0800

    Derrr

--- .travis.before_install.sh
+++ .travis.before_install.sh
@@ -5,6 +5,7 @@ sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com AEF0CF8E
 
 # Add the repo
 #TODO make this match the branch
-sudo add-apt-repository -y apt-deb http://ubuntu.adiscon.com/v7-stable precise/
+sudo add-apt-repository -y "deb http://ubuntu.adiscon.com/v7-stable precise/"
 
+sudo apt-get update
 sudo apt-get install liberstr
